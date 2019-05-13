316841b9d68ac7823c47c802fecf2ee9c1f2f6fd
Harald Maier <43604893+maierh7@users.noreply.github.com>
2019-05-13 13:51:05 +0200

    Added list backups
    Added additional options: --back, inst

--- py/sql.py
+++ py/sql.py
@@ -4,10 +4,13 @@ import opt
 import sub
 
 opt.parser.add_option ("", "--list", default=False, action="store_true", dest="list")
+opt.parser.add_option ("", "--back", default=False, action="store_true", dest="back")
+opt.parser.add_option ("", "--inst", dest="inst")
 
 (opts, args) = opt.GetOptions ()
 
-if opts.list:
+if opts.back == True and opts.list == True and opts.inst:
+    sub.out_cmd ("gcloud sql backups list -i " + opts.inst)
+elif opts.list:
     sub.out_cmd ("gcloud sql instances list")
-
 
