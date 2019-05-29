2a7683d2e893dd70382ddf4aad66b9c9226ef0dc
Harald Maier <maierha@nyx.localdomain>
2019-05-29 15:26:04 +0200

    Additional packages for python
    Added additional packages for python build.

--- sh/yum-init.sh
+++ sh/yum-init.sh
@@ -4,5 +4,10 @@ yum install emacs
 yum install autoconf
 yum install texinfo
 yum install ncurses-devel
+
 yum install gcc
 yum install git
+
+# For python install
+yum install zlib-devel
+yum install libffi-devel
