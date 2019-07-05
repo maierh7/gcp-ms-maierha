c6bd8c5d68485dd870344c26b350b7f12ce4c1c6
maierh7 <43604893+maierh7@users.noreply.github.com>
2019-07-05 12:19:57 +0200

    Added Delete Option and Function

--- py/backup-pg.py
+++ py/backup-pg.py
@@ -8,7 +8,8 @@ from oauth2client.client import GoogleCredentials as client
 
 opt.parser.add_option ("", "--proj",  dest="proj")
 opt.parser.add_option ("", "--inst",  dest="inst")
-opt.parser.add_option ("", "--print", dest="print", default=False, action="store_true")
+opt.parser.add_option ("", "--delete",  dest="delete", default=False, action="store_true")
+opt.parser.add_option ("", "--print",   dest="print",  default=False, action="store_true")
 
 (opts, args) = opt.GetOptions ()
 
@@ -23,5 +24,7 @@ sql = sqladm.SQLAdm (opts.proj, opts.inst, cred)
 if opts.print:
     sql.print_version ()
     sql.print_blst ()
+elif opts.delete:
+    sql.delete_backup_less_24 ()
 else:
     sql.do_backup ()
