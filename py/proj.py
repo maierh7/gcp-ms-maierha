f9fff82e259ee50478f6a60a74b151cabc19874a
Harald Maier <maierha@mediamarktsaturn.com>
2019-08-14 16:54:01 +0200

    Added --sql Option

--- py/proj.py
+++ py/proj.py
@@ -8,14 +8,23 @@ import opt
 import sub
 import subprocess
 
+opt.RemoveServ ()
 opt.parser.add_option ("-l", "--list", default=False, action="store_true", dest="list")
 opt.parser.add_option ("-c", "--cur",  default=False, action="store_true", dest="cur")
 opt.parser.add_option ("-a", "--acc",  default=False, action="store_true", dest="acc")
 opt.parser.add_option ("-p", "--pro",  dest="pro")
+opt.parser.add_option ("-s", "--sql",  default=False, action="store_true", dest="sql")
 
 (opts, args) = opt.GetOptions ()
 lst = dict ()
 cur = ""
+
+sqlp = {
+    "V135-5256-Playground-HaraldMai" : "v135-5256-playground-haraldmai",
+    "V135-playground-mai-dev" :        "xbbcvpbxkziv",
+    "6117V135-myaccount-prod" :        "aq12fq1g0adk",
+    "6117V135-myaccount-dev"  :        "u1f3rwmxbxdt",
+    }
 
 def get_cur_pro ():
     out = subprocess.check_output ("gcloud config get-value project", 
@@ -42,7 +51,21 @@ def set_project ():
     out = subprocess.check_output ("gcloud config get-value project", shell=True).decode ('utf-8')
     lines = out.splitlines ()
     for i in lines:
-        print (lst[i])
+        print (i)        
+
+def get_sql_project ():
+    idx = 0
+    for i in sqlp:
+        idx = idx +1
+        print ("%2d %30s %30s" % (idx, i, sqlp[i]))
+    sel = int (input ("Get index: "))
+    pro = list (sqlp.keys ())[sel-1]
+    opts.pro = sqlp[pro]
+    set_project ()
+        
+if opts.sql:
+    get_sql_project ()
+    sys.exit (0)
 
 get_cur_pro ()
 get_projects ()
@@ -70,4 +93,3 @@ if opts.acc:
 
 if opts.pro:
     set_project ()
-    
