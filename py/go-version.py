c30c32209e75714fbc42170afbbaae940f54ad20
maierh7 <maierha@media-saturn.com>
2019-10-09 14:11:29 +0000

    Added try/ecept

--- py/go-version.py
+++ py/go-version.py
@@ -1,9 +1,14 @@
 #! /usr/bin/env python3
 
+import sys
 import subprocess
 
-out = subprocess.check_output ("type -p go", shell=True, executable="/bin/bash").decode("utf-8")
-lin = out.splitlines ()
+try:
+    out = subprocess.check_output ("type -p go", shell=True, executable="/bin/bash").decode("utf-8")
+    lin = out.splitlines ()
+except subprocess.CalledProcessError as e:
+    sys.exit ()
 
 for i in lin:
     subprocess.run ("go version", shell=True)
+        
