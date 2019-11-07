df0455e38cfc84290c13c22381556cc5c5b2e269
Harald Maier <maierha@mediamarktsaturn.com>
2019-11-07 14:54:56 +0100

    Added All Option
    This lists all SQL instances accessible and the count of backups of
    the instance.

--- py/lib/sql.py
+++ py/lib/sql.py
@@ -1,27 +1,51 @@
 
+import sys
+from enum import Enum
+
 from oauth2client.client import GoogleCredentials as client    
 from googleapiclient import discovery
 from googleapiclient.errors import HttpError
 
+class SQL_Status (Enum):
+    LIST  = 0
+    EMPTY = 1
+    PERM  = 2
+
 class SQL:
+    
     sql = None
     proj = None
     items = None
+
+    status = SQL_Status.LIST
     
     def __init__ (self, proj, credentials):
         self.proj = proj
         self.sql = discovery.build ("sqladmin", "v1beta4", credentials=credentials)
         self.items = list ()
-        self.get_items ()
+        self.status = self.get_items ()
 
     def get_items (self):
 
         req = self.sql.instances().list (project=self.proj)
         while req:
-            res = req.execute ()
+            try:
+                res = req.execute ()
+            except HttpError as err:
+                if err.resp.status in [403]:
+                    return SQL_Status.PERM
+                print (self.proj)
+                print ("HTTP-Error: ", err.resp.status)
+                sys.exit ()
+                
+            if 'items' not in res:
+                # No SQL instances
+                return SQL_Status.EMPTY
+            
             for i in res['items']:
                 self.items.append (i)
             req = self.sql.instances().list_next (previous_request=req, previous_response=res)
+            return SQL_Status.LIST
 
     def list (self):
         lst = list ()
@@ -38,3 +62,19 @@ class SQL:
             res [inst] = sama
         return res
     
+    def get_backup_count (self, inst, cred):
+        adm = discovery.build ("sqladmin", "v1beta4", credentials=cred)
+
+        ind = 0
+        req = adm.backupRuns().list (project=self.proj, instance=inst)
+        while req:
+            res = req.execute ()
+
+            if "items" not in res:
+                continue
+
+            for i in res["items"]:
+                ind = ind + 1
+            req = adm.backupRuns().list_next (previous_request=req, previous_response=res)
+        return ind
+
--- py/sqlapi.py
+++ py/sqlapi.py
@@ -1,17 +1,29 @@
 #! /usr/bin/env python3
 
+import time
+
 from oauth2client.client import GoogleCredentials as client    
 from googleapiclient import discovery
 from googleapiclient.errors import HttpError
 
-from lib.sql import SQL
+from lib.sql    import SQL
+from lib.sql    import SQL_Status
+from lib.pro    import Project
+from lib.sqladm import SQLAdm
 
 import lib.opt as opt
 
-opt.parser.add_option ("", "--sa", default=False, action="store_true", dest="sa")
+opt.parser.add_option ("", "--sa",  default=False, action="store_true", dest="sa")
+opt.parser.add_option ("", "--all", default=False, action="store_true", dest="all")
+opt.parser.add_option ("", "--pro", dest="pro")
 
 (opts, args) = opt.GetOptions ()
 
+excl = (
+    # "vvvcxwdwj1xa",
+    # "sys-48609460970177310285461281"
+    )
+ 
 sqlp = {
     "V135-5256-Playground-HaraldMai" : "v135-5256-playground-haraldmai",
     "V135-playground-mai-dev" :        "xbbcvpbxkziv",
@@ -21,7 +33,36 @@ sqlp = {
 
 cred = client.get_application_default()
 
-if opts.sa:
+if opts.all:
+    res = dict ()
+    res [SQL_Status.LIST]  = 0
+    res [SQL_Status.EMPTY] = 0
+    res [SQL_Status.PERM]  = 0
+    
+    pro  = Project (cred)
+    print ("Project-Count", len (pro.plst))
+    for i in pro.plst:
+        if opts.pro is not None:
+            if opts.pro != pro.plst[i]:
+                continue
+        pid = pro.plst[i]
+
+        if pid in excl:
+            continue
+        sql = SQL (pid, cred)
+        res [sql.status] += 1
+        if sql.status == SQL_Status.LIST:
+            lst = sql.list ()
+            print ("%1d %30s %s" % (len (lst), pid, i))
+            for j in lst:
+                cnt = sql.get_backup_count (j, cred)
+                print ("  %2d %s" % (cnt, j))
+
+    print ("Project-Count", len (pro.plst))
+    for i in res:
+        print (i, res[i])
+            
+elif opts.sa:
     for i in sqlp:
         sql = SQL (sqlp[i], cred)
         sa = sql.get_sa ()
